927593149fe2d520c446e9d1e393af9abe4ee298
AprilGong <volero458@gmail.com>
2023-02-05 17:54:59 +0900

    implement `<ApiButton/>` and generic component `<SplitButton/>`, `<SetSrcPathButton/>`, `<SetOutputDirButton/>`

--- src/components/molecules/Button/ApiButton.tsx
+++ src/components/molecules/Button/ApiButton.tsx
@@ -1,42 +1,69 @@
 import { Button } from "@mui/material"
+import { ipcRenderer } from 'electron';
 import trySplit from "../../../api/Split"
-import { ApiStatus, Bookmark, ChildResponse, Error } from "typedefs/types"
-import { isBookmarks, isOutputPathLike, isSrcPathLike } from '../../../utils/Typeguards';
+import trySetPathOf from "../../../api/SetPathOf";
+import { ApiStatus, Bookmark, Error, Maybe, PathLike } from "typedefs/types"
+import { isPathLike, isBookmarks } from '../../../utils/Typeguards';
+import { MarkerNameUtil, TimeCodeUtil } from "../../../utils/Utilities";
 
 interface ApiButtonProps<T> {
     arg: T,
     label: string,
-    disabled: boolean,
-    onClick: (apiStatus: ApiStatus) => void,
-    onSuccess: () => any,
+    disabled?: boolean,
+    onSuccess: (arg: ApiStatus | PathLike) => void,
     onFail: (err: Error) => void,
+    onPending?: (arg: ApiStatus) => void
 }
-
 export type TypedApiButton<ArgumentType> = React.FC<ApiButtonProps<ArgumentType>>;
 
-export function ApiButton<T>({arg, label, onClick, onSuccess, onFail}: ApiButtonProps<T>) {
-    const clickHandler = async (): Promise<void> => {
-        if ((Array.isArray(arg)) && isBookmarks(arg)) {
-            onClick('splitting');
-            try {
-                await trySplit(arg);
-                onSuccess();
-            }
-            catch (err) {
-                onFail((err as ChildResponse).error!);
-            }
-        }
-        else {
-            console.log('(((');
-        }
+export function ApiButton<T>({arg, label, disabled, onSuccess, onFail, onPending}: ApiButtonProps<T>) {
+    const fileDialogPickHandler = async (picked: PathLike): Promise<void> => {
+        const { path, kind } = picked;
+        const response = await trySetPathOf(kind, path);
+        onSuccess({ __typename: 'PathLike', path: response, kind: kind });
+    };
+
+    const postProcessBeforeSplit = (bookmark: Bookmark): Bookmark => {
+        let { bookmarkName } = bookmark;
+        const { begin, end } = bookmark.marker;
+
+        begin.markerTime = TimeCodeUtil.mask(begin.markerTime);
+        end.markerTime = TimeCodeUtil.mask(end.markerTime);
+        bookmarkName = MarkerNameUtil.sanitizeMarerkName(bookmarkName);
+
+        return { bookmarkName, marker: { begin, end } };
     }
+    
+    const clickHandler = async (): Promise<void> => {
+        if (isPathLike(arg)) {
+            const { kind } = arg;
+            const maybePath: Maybe<string> = await ipcRenderer.invoke(kind === 'src' ? 'picksrc': 'pickdir');
+            if (maybePath) {
+                fileDialogPickHandler({ path: maybePath, kind: kind });
+            }
+        }
+        else if ((Array.isArray(arg)) && isBookmarks(arg)) {
+            onPending!('splitting');
+
+            const response = await trySplit(arg.map(postProcessBeforeSplit));
+            if (response.error) {
+                onFail(response.error);
+            }
+            else {
+                onSuccess('fulfilled');
+            }
+        }
+    };
 
     return (
-        <Button 
-            size={'large'}
-            onClick={clickHandler}
-        >
-            { label }
-        </Button>
+        <>
+            <Button 
+                size={'large'}
+                onClick={clickHandler}
+                disabled={disabled}
+            >
+                { label }
+            </Button>
+        </>
     )
 }
--- src/components/molecules/Button/index.tsx
+++ src/components/molecules/Button/index.tsx
@@ -1,4 +1,6 @@
 import { Bookmark, PathLike } from 'typedefs/types';
 import { TypedApiButton, ApiButton } from './ApiButton';
 
-export const SplitApiButton: TypedApiButton<Bookmark[]> = (props) => { return <ApiButton {...props}/> };
+export const SplitButton: TypedApiButton<Bookmark[]> = (props) => { return <ApiButton {...props}/> };
+export const SetSrcPathButton: TypedApiButton<PathLike> = (props) => { return <ApiButton {...props}/> };
+export const SetOutputDirButton: TypedApiButton<PathLike> = (props) => { return <ApiButton {...props}/> };
