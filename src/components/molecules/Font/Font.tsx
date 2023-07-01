14f4144069ef3f966eca8deeefef0745da0836d4
AprilGong <volero458@gmail.com>
2023-07-01 18:04:42 +0900

    minor

--- src/components/molecules/Font/Font.tsx
+++ src/components/molecules/Font/Font.tsx
@@ -1,10 +1,9 @@
 import { StyledFont } from "./Font.style";
-import type { ErrorLevel } from "typedefs/types";
 import Tooltip, { TooltipProps } from "@mui/material/Tooltip/Tooltip";
 
 export type FontType = 'h1' | 'h2' | 'h3' | 'h4' | 'body' | 'label';
 export type FontWeight = 'bold' | 'italic';
-export type FontStatus = ErrorLevel | 'NORMAL' | 'ACTIVATED' | 'DISABLED';
+export type FontStatus = "WARNING" | "CRITICAL" | 'NORMAL' | 'ACTIVATED' | 'DISABLED';
 
 export interface FontProps {
     label: string,
